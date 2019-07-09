7ce2fda4609e4afc848f13c3375e3ae514920896
weizheng <wz_ling1991@sina.com>
2019-07-09 23:33:53 +0800

    feign

--- feign-consumer/src/main/java/wz_ling1991/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/wz_ling1991/FeignConsumerApplication.java
@@ -1,13 +1,17 @@
 package wz_ling1991;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-//@EnableFeignClients
-//@EnableEurekaClient
-//@SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
 public class FeignConsumerApplication {
 
   public static void main(String[] args) {
-//    SpringApplication.run(FeignConsumerApplication.class, args);
+    SpringApplication.run(FeignConsumerApplication.class, args);
   }
 }
--- feign-consumer/src/main/java/wz_ling1991/configuration/FeignLogConfiguration.java
+++ feign-consumer/src/main/java/wz_ling1991/configuration/FeignLogConfiguration.java
@@ -1,15 +1,15 @@
-//package wz_ling1991.configuration;
-//
-//import feign.Logger;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class FeignLogConfiguration {
-//
-//  @Bean
-//  Logger.Level feignLoggerLevel() {
-//    return Logger.Level.FULL;
-//  }
-//
-//}
+package wz_ling1991.configuration;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignLogConfiguration {
+
+  @Bean
+  Logger.Level feignLoggerLevel() {
+    return Logger.Level.FULL;
+  }
+
+}
